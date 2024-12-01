e5d7b99c6bcb3c217d94753aeb360f5fa79f52b6
elvis-v12 <elvisvegaucv@gmailcom>
2024-11-30 21:48:12 -0500

    Eliminar todos los archivos de la rama main

--- cliente/src/js/rutas.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("more-courses").addEventListener("click", function() {
-    alert("Mostrando más cursos...");
-    // Aquí puedes agregar más lógica si quieres cargar más cursos dinámicamente.
-});
