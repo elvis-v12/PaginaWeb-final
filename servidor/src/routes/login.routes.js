db3a6b264c45937ede111724242408b25d0ee12d
Brandon Lee Roque Mendoza <broqueme@ucvvirtual.edu.pe>
2024-12-08 17:18:27 -0500

    probando

--- servidor/src/routes/login.routes.js
+++ servidor/src/routes/login.routes.js
@@ -3,7 +3,7 @@ import { iniciarSesion } from '../controllers/login.controller.js';
 
 const router = express.Router();
 
-// Ruta para iniciar sesión
+// Ruta para iniciar sesión jj
 router.post('/login', iniciarSesion);
 
 export default router;
